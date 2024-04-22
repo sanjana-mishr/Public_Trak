0669db854a8fdb6b94e5f144d7fb474485cc1882
sanjana <sanjana.mishra@learner.manipal.edu>
2024-04-22 10:49:56 +0530

    frfgd

--- app/src/main/java/com/example/public_trak/HomePage.kt
+++ app/src/main/java/com/example/public_trak/HomePage.kt
@@ -1,12 +1,14 @@
 package com.example.public_trak
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.Window
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -14,31 +16,55 @@ import com.google.android.material.navigation.NavigationView
 
 class HomePageActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var switchMode: SwitchCompat
+    private var nightMode: Boolean = false
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this)
 
         val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
-
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
+        switchMode = findViewById(R.id.switchMode)
+        sharedPreferences = getSharedPreferences("myPrefs", MODE_PRIVATE)
 
+        nightMode = sharedPreferences.getBoolean("nightMode", false)
+
+        if (nightMode) {
+            switchMode.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
+        switchMode.setOnClickListener {
+            nightMode = !nightMode
+            if (nightMode) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            editor = sharedPreferences.edit()
+            editor.putBoolean("nightMode", nightMode)
+            editor.apply()
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.nav_home -> {val intent = Intent(this, HomePageActivity::class.java)
-            startActivity(intent)}
+            R.id.nav_home -> {
+                val intent = Intent(this, HomePageActivity::class.java)
+                startActivity(intent)
+            }
             R.id.nav_about -> supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, AboutFragment()).commit()
             R.id.nav_logout -> Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show()
@@ -48,11 +74,10 @@ class HomePageActivity : AppCompatActivity(), NavigationView.OnNavigationItemSel
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START)
         } else {
-            onBackPressedDispatcher.onBackPressed()
+            super.onBackPressed()
         }
     }
 }
--- app/src/main/java/com/example/public_trak/LoginActivity.kt
+++ app/src/main/java/com/example/public_trak/LoginActivity.kt
@@ -2,13 +2,13 @@ package com.example.public_trak
 
 import android.content.Intent
 import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.example.public_trak.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -32,7 +32,7 @@ class LoginActivity : AppCompatActivity() {
 
                 firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
                     if (it.isSuccessful){
-                        val intent = Intent(this, HomePage::class.java)
+                        val intent = Intent(this, HomePageActivity::class.java)
                         startActivity(intent)
                     } else {
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
